b8b4651ffdd84c3a19358db44705b380557ffbdb
Jordy <s1195262@student.windesheim.nl>
2025-08-12 15:44:07 +0200

    first push

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Festival Travel System</title>
+</head>
+<body>
+    <h1>Welkom bij Festival Travel System</h1>
+    <p>Dit is de startpagina van jouw project.</p>
+</body>
+</html>
